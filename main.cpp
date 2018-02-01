6d0426b86a8496bed2691da0a49c195c565a643d
Sami Dridi <sami.dridi92@gmail.com>
2018-02-02 00:20:03 +0900

    fix main

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    UARTConnection uartConnection = new UARTConnection();
-    uartConnection.connect();
+    UARTConnection* uartConnection = new UARTConnection();
+    uartConnection->connect();
 
     while(true) {
         string data;
         cout << "Please enter a message: " << endl;
         cin >> data;
 
-        uartConnection.send(data.c_str(), data.length());
+        uartConnection->send(data.c_str(), data.length());
     }
     return 0;
 }
